0e6c72d6dea4e7f0d8e9c2eebc421bf8e22ac59d
Corfaya <lauracorrado0696@gmail.com>
2024-09-18 16:33:31 +0200

    [UPD] Child class "Kennel" created

--- index.php
+++ index.php
@@ -72,6 +72,25 @@
         }
     }
 
+    class Kennel extends Item {
+        public $description;
+        public $size;
+        public $isIndoor;
+
+        public function __construct($_image, $_title, $_price, Category $_category, $_type, $_description, $_size, $_isIndoor)
+        {
+            parent::__construct($_image, $_title, $_price, $_category, $_type);
+            $this->description = $_description;
+            $this->size = $_size;
+            $this->isIndoor = $_isIndoor;
+        }
+
+        public function getInfos() {
+            $inOrOut = $this->isIndoor ? "SOLO PER INTERNI" : "SOLO PER ESTERNI";
+            return $this->description." ".$this->size." cm. $inOrOut";
+        }
+    }
+
     // objects from Category
     $dog = new Category("icona", "cane");
     $cat = new Category("icona", "gatto");
@@ -85,6 +104,10 @@
     // object from Toy
     $dogToy = new Toy("https://placehold.co/600x400?text=Gioco+cani", "Osso antimicrobico", "4,00", $dog, "gioco", "Giocattolo da masticazione. Nel giocattolo sono presenti additivazioni antimicrobiche con efficacia del 99,9%.");
     var_dump($dogToy->getInfos());
+
+    // object from Kennel
+    $dogHouse = new Kennel("https://placehold.co/600x400?text=Cuccia+cane", "Cuccia rettangolare", "35,00", $dog, "cuccia", "Cuccia dal design moderno e leggero, di forma rettangolare con cuscino estraibile e imbottita di fibra di poliestere.", "100x900", true);
+    var_dump(($dogHouse->getInfos()));
 
     // objects from Item
     $dogFood = new Item("https://placehold.co/600x400?text=Cibo+cani", "Croccantini per cani", "12,50", $dog, "cibo");
